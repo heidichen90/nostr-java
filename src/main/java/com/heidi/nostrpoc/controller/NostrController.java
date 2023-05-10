8873601ad0f66bade17ba82932f9d9b85d1b6bf7
jerry80409 <jerry80409@gmail.com>
2023-05-10 23:38:30 +0800

    fix. NostController
    I think NostContoller will make you confusion, so I rename it

--- src/main/java/com/heidi/nostrpoc/controller/NostrController.java
+++ src/main/java/com/heidi/nostrpoc/controller/NostrController.java
@@ -7,17 +7,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * I think NostContoller will make you confusion, so I rename it to RESTful Controller.
+ * In this controller, I expected it should hande the HTTP Request not webSocket Request.
+ * <p>
+ * The process flow Just like this
+ * <p>
+ * client -> NostrController -> websocket -> WebsocketHandler
+ * <p>
+ * so this class we dont need the NostrWebSocketHandler,
+ * we just need injection the NostrWebSocketClient help me send the websocket message.
+ */
 @RestController
 @Slf4j
 public class NostrController {
 
-    private NostrWebSocketHandler handler = new NostrWebSocketHandler();
-    private NostrWebSocketClient client = new NostrWebSocketClient(handler);
+//    private NostrWebSocketHandler handler = new NostrWebSocketHandler();
 
+//    private NostrWebSocketClient client = new NostrWebSocketClient(handler);
+
+    // todo - I will fix the WebSocketClient before.
     @GetMapping("/nostr/hello")
     public String sayHello() throws JsonProcessingException {
-        client.createSession();
-        handler.sendNostrEvent(client.getSession());
+//        client.createSession();
+//        handler.sendNostrEvent(client.getSession());
         return "Hello Nostr!";
     }
 }
