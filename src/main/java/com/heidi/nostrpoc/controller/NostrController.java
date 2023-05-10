801277375c5c4b5627e265f2a12bb3d5168a9832
jerry80409 <jerry80409@gmail.com>
2023-05-11 00:49:18 +0800

    fix. NsotrController

--- src/main/java/com/heidi/nostrpoc/controller/NostrController.java
+++ src/main/java/com/heidi/nostrpoc/controller/NostrController.java
@@ -1,10 +1,20 @@
 package com.heidi.nostrpoc.controller;
 
+import static java.time.ZoneOffset.UTC;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.heidi.nostrpoc.client.NostrWebSocketClient;
 import com.heidi.nostrpoc.client.NostrWebSocketHandler;
+import com.heidi.nostrpoc.client.SimpleWebSocketClient;
+import com.heidi.nostrpoc.constant.NostrEvent;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,17 +30,45 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class NostrController {
+
+    private static final String PUBLIC_KEY = "e0131db0689078f518710ad970a2b37f7e1af28769238a447cf95455df72eb4a";
 
 //    private NostrWebSocketHandler handler = new NostrWebSocketHandler();
 
 //    private NostrWebSocketClient client = new NostrWebSocketClient(handler);
 
-    // todo - I will fix the WebSocketClient before.
+    private final SimpleWebSocketClient simpleWebSocketClient;
+
+    private final ObjectMapper objectMapper;
+
+
+    // you can design the API to send Nostr event by
     @GetMapping("/nostr/hello")
     public String sayHello() throws JsonProcessingException {
 //        client.createSession();
 //        handler.sendNostrEvent(client.getSession());
+
+        // I think Record will be easier
+        NostrEvent event = new NostrEvent();
+        event.setKind(1);
+        event.setPubkey(PUBLIC_KEY);
+        event.setCreatedAt(LocalDateTime.now().toEpochSecond(UTC));
+        event.setTags(List.of("test"));
+        event.setContent("Hello from Heidi");
+        event.setId(event.generateId());
+        event.setSig(null);
+
+        List<Object> list = new ArrayList<>();
+        list.add("EVENT");
+        list.add(event);
+
+        final String json = objectMapper.writeValueAsString(list);
+
+        simpleWebSocketClient.syncSendMessage(json);
+
+        // we dont need close, because it will handle another request the send the Nostr event.
         return "Hello Nostr!";
     }
 }
